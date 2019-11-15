86be6505af5bc1ae3f4e6d80dc3096be4f286324
himanshu <himanshusharma.it.11@gmail.com>
2019-11-15 23:37:31 +0530

    jenkins updated

--- renameFile.js
+++ renameFile.js
@@ -2,16 +2,17 @@ const fs = require('fs');
 
 const buildNo = process.argv[2];
 
-
+fs.rename('./build/index.html', './build/index-'+buildNo+'.html', function (err, success) {
+    if (err) {
+        console.log('error while renaming index.html', err);
+    }
+});
 console.log('buildNo', buildNo);
 
 
 /***
 
-stage('Deploy to S3') {
-    steps {
-        bat 'aws s3 cp build s3://resume-viewer/build-${BUILD_NUMBER} --recursive'
-    }
-}
+ bat 'aws s3 cp build s3://resume-viewer/build-${BUILD_NUMBER} --recursive'
+ bat 'aws cloudfront update-distribution --id E343P8PQ0PKMAQ \ --default-root-object index${-${BUILD_NUMBER}}.html'
 
 */
